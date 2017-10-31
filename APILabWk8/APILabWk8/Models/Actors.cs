f5ecb05a821e47cc1f91e9c8df3bc656a686bb8f
Gregory Dukes <kcils360@gmail.com>
2017-10-31 09:40:27 -0700

    built actors model

---
+++ APILabWk8/APILabWk8/Models/Actors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILabWk8.Models
+{
+    public class Actors
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
