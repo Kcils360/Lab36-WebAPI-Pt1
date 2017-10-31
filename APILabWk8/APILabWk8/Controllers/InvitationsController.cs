f414d2ed1835ff3cfef6afb9f507f51df8e159bb
Gregory Dukes <kcils360@gmail.com>
2017-10-31 10:18:50 -0700

    created dbContext and started controller for invitations

---
+++ APILabWk8/APILabWk8/Controllers/InvitationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APILabWk8.Controllers
+{
+    public class InvitationsController : ControllerBase
+    {
+    }
+}
